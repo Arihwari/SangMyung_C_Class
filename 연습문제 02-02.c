400fa941d49e041fd4cbcde19cdced123ea4bd06
AriHwari <dksrytn97@naver.com>
2021-09-07 21:42:08 +0900

    연습문제 02-02

---
+++ 연습문제 02-02.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	printf("이    름: 안 교 수\n");
+	printf("주    소: 충청남도 천안시 동남구 청수로 15\n");
+	printf("전화번호: 010-1234-5678\n");
+	return 0;
+}
--- 홍길동.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("È«±æµ¿\n");
-	printf("È« ±æ µ¿\n");
-	printf("È«  ±æ  µ¿\n");
-	return 0;
-}
