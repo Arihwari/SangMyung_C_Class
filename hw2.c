4d98517d26ca813af74b91b15f4e1d620ab5bcc8
AriHwari <dksrytn97@naver.com>
2021-09-26 23:34:57 +0900

    hw2.c

---
+++ hw2.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	double kil_to_mile, kil;
+
+	printf("Please enter kilometers: ");
+	scanf("%lf", &kil);
+
+	kil_to_mile = kil / 1.609;
+	printf("%.1f km is equal to %.1f.", kil, kil_to_mile);
+	return 0;
+}
