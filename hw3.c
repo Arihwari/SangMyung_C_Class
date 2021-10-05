5b9afdf9ab54bae6c8c9f12210d7fa69540ea094
AriHwari <dksrytn97@naver.com>
2021-10-05 18:47:09 +0900

    hw3.c

---
+++ hw3.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int ip_num;
+
+	printf("Please enter a number: ");
+	scanf("%d", &ip_num);
+	if (ip_num % 2 == 0)
+		printf("It is not a prime number");
+	else
+		printf("It is a prime number");
+
+	return 0;
+
+}
