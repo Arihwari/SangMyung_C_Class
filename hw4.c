4cd20c35042afce90ed68af1743a0efe168f7d86
AriHwari <dksrytn97@naver.com>
2021-10-12 13:04:39 +0900

    hw4.c

---
+++ hw4.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+void returnfunc(int num)
+{
+	int a;
+	a = num % 2;
+	if (num > 1)
+		returnfunc(num / 2);
+
+	printf("%d", a);
+	return;
+}
+int main(void)
+{
+	int num;
+	printf("Please enter a number: ");
+	scanf("%d", &num);
+	returnfunc(num);
+
+	return 0;
+}
